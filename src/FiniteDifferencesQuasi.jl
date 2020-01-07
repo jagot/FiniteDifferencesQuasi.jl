411216674c2ed0cf8f7796d242c93178cb3da774
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-01-07 13:01:35 +0100

    Support for restricted bases

--- src/FiniteDifferencesQuasi.jl
+++ src/FiniteDifferencesQuasi.jl
@@ -9,7 +9,7 @@ import ContinuumArrays: Basis, ℵ₁, Derivative, Inclusion, @simplify
 using QuasiArrays
 import QuasiArrays: AbstractQuasiMatrix, QuasiAdjoint, MulQuasiArray,
     PInvQuasiMatrix, InvQuasiMatrix, QuasiDiagonal,
-    BroadcastQuasiArray
+    BroadcastQuasiArray, SubQuasiArray
 
 using IntervalSets
 
@@ -18,9 +18,12 @@ using LazyArrays
 using LinearAlgebra
 import LinearAlgebra: Matrix, dot
 using SparseArrays
+using BandedMatrices
+using FillArrays
 
 using Printf
 
+include("restricted_bases.jl")
 include("finite_differences.jl")
 include("inner_products.jl")
 include("operators.jl")
